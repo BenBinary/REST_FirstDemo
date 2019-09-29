c78060765bade79cfdb6c4552e5d065661babf1a
BenBinary94 <bk191414@gmail.com>
2019-09-29 22:55:59 +0200

    noch Probleme beim Einlesen von React Native

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ exports.getPosts = (req, res, next) => {
     
         res.status(200).json({
             posts: [{
+                _id: '1',
                 title: "Entenpost", 
                 content: "This is the first post",
                 image: '/image/duck.jpg',
@@ -19,14 +20,24 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
 
+    console.log("req " + req.body);
+
     console.log("Titel und Content", title, content);
 
     // Create post in DB
     res.status(201).json({
 
         message: 'Post successfully created',
-        post: {id: "asdf", title: title, content: content}
-
+        post: { 
+            _id: new Date().toISOString, 
+            title: title, 
+            content: content,
+            creator: {
+                name: "Benedikt Kurz"
+            },
+            createdAt: new Date()
+        
+        }
     }); 
 
 };
